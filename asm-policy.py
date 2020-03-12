dd2f42fb67028e8b746af4aa35a5fc0d576054f2
q2807c <q2807c@gmail.com>
2020-03-12 17:01:28 +0800

    first commit

---
+++ asm-policy.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+url = "https://10.200.159.170/mgmt/tm/asm/policies/"
+headers = {"Content-Type": "application/json"}
+r = requests.get(url, headers=headers, auth=HTTPBasicAuth('admin', 'admin'), verify=False).json()
+json_data = r.get('items')
+
+print("Policy_Name", "\t", "Hash_Value")
+for item in range(len(json_data)):
+    policy = json_data[item]
+    name = policy.get('name')
+    hash = policy.get('plainTextProfileReference').get('link').split("/", 9)[7]
+    print(name, "\t", hash)
+
+
+
+
+
