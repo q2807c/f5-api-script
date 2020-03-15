1861bd39404ff51509eedf132c29f8c5c9f98527
q2807c <q2807c@gmail.com>
2020-03-15 13:39:22 +0800

    Check if the VS has pool configured

--- vs-pool.py
+++ vs-pool.py
@@ -14,27 +14,36 @@ requests.packages.urllib3.disable_warnings()
 response = requests.get(url_vs, headers=headers, auth=HTTPBasicAuth('admin', '%s' % password), verify=False).json()
 response_data = response.get('items')
 
+
 print("VS名称", "------->", "VS IP端口", "--------------->", "pool名称", "------->", "pool members")
 
 # 循环获取每个VS以及对应pool的信息
 
 for config in response_data:
+    pool_lst = []
     vs = config['name']
     destination = config['destination']
-    pool = config['pool']
 
-    # 循环获取pool member信息
-    pool = repr(pool).replace('/', '~').strip('\'')  # 适配API格式，将/替换为~
-    pool_url = "https://%s/mgmt/tm/ltm/pool/%s/members" % (bigip,pool)
-    pool_response = requests.get(pool_url, headers=headers, auth=HTTPBasicAuth('admin', '%s' %password), verify=False).json()
-    pool_response = pool_response.get('items')
-    #定义列表存放pool member信息
-    pool_lst = []
-    for pool_member in pool_response:
-        member = pool_member['fullPath']
-        pool_lst.append(member)
+    # 判断VS是否关联了pool
 
-    pool = repr(pool).replace('~', '/').strip('\'')  # 将~替换回/
+    if "pool" in config:
+        pool = config['pool']
+        # 循环获取pool member信息
+        pool = repr(pool).replace('/', '~').strip('\'')  # 适配API格式，将/替换为~
+        pool_url = "https://%s/mgmt/tm/ltm/pool/%s/members" % (bigip, pool)
+        pool_response = requests.get(pool_url, headers=headers, auth=HTTPBasicAuth('admin', '%s' % password),
+                                     verify=False).json()
+        pool_response = pool_response.get('items')
+        # 定义列表存放pool member信息
+        for pool_member in pool_response:
+            member = pool_member['fullPath']
+            pool_lst.append(member)
+
+        pool = repr(pool).replace('~', '/').strip('\'')  # 将~替换回/
+
+    else:
+        pool = 'No_Pool_In_Configuration'
+
 
     print( vs, "--->", destination, "--->", pool, "--->", pool_lst)
 
